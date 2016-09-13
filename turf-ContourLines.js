d95c6925890b008d7d2c2c7b750f9043dd4f5491
HichamZouarhi <hicham.zouarhi@gmail.com>
2016-09-13 14:35:31 +0100

    took off Nan and duplicate coordinates from the vertices of the contour lines

--- turf-ContourLines.js
+++ turf-ContourLines.js
@@ -1,15 +1,15 @@
-function calculateTIN() {
+function calculateTIN(vertices) {
 	// generate some random point data
-	var vertices = turf.random('points', 30, {
+	/*var vertices = turf.random('points', 30, {
 		bbox: [-10, 20, 10, 40]
-	});
+	});*/
 	//var vertices=turf.featureCollection(points);
 	//console.log(JSON.stringify(vertices));
 	//=points
 	// add a random property to each point between 0 and 9
-	for (var i = 0; i < vertices.features.length; i++) {
+	/*for (var i = 0; i < vertices.features.length; i++) {
 		vertices.features[i].properties.z = ~~(Math.random() * 9);
-	}
+	}*/
 
 	var min=vertices.features[0];
 	for(var i = 1; i < vertices.features.length; i++){
@@ -44,22 +44,24 @@ function generateContourLines(tin, min, max){
 			var coords;
 			if((z>=edges.a && z<=edges.b) || (z<=edges.a && z>=edges.b)){
 				coords=getCoordsForZ(z, tin.features[i].geometry.coordinates[0][0], tin.features[i].geometry.coordinates[0][1], edges.a, edges.b);
-				sameLevelPoints.push(turf.point(coords, {"z": z}));
+				if(coords[0] && coords[1]) sameLevelPoints.push(turf.point(coords, {"z": z}));
 			}
 			if((z>=edges.b && z<=edges.c) || (z<=edges.b && z>=edges.c)){
 				coords=getCoordsForZ(z, tin.features[i].geometry.coordinates[0][1], tin.features[i].geometry.coordinates[0][2], edges.b, edges.c);
-				sameLevelPoints.push(turf.point(coords, {"z": z}));
+				if(coords[0] && coords[1]) sameLevelPoints.push(turf.point(coords, {"z": z}));
 			}
 			if((z>=edges.c && z<=edges.a) || (z<=edges.c && z>=edges.a)){
 				coords=getCoordsForZ(z, tin.features[i].geometry.coordinates[0][2], tin.features[i].geometry.coordinates[0][3], edges.c, edges.a);
-				sameLevelPoints.push(turf.point(coords, {"z": z}));
+				if(coords[0] && coords[1]) sameLevelPoints.push(turf.point(coords, {"z": z}));
 			}
 		}
-		verticesContourLines[z]=sameLevelPoints;
-		//console.log(JSON.stringify(sameLevelPoints));
+		var uniqueVertices=verticesUnique(sameLevelPoints);
+		verticesContourLines[z]=uniqueVertices;
+		//console.log(JSON.stringify(uniqueVertices));
+		//console.log("Height : "+z+" number of points  "+uniqueVertices.length);
 		var coordContourLines=[];
-		for(var i=0; i<sameLevelPoints.length ; i++){
-			coordContourLines.push(sameLevelPoints[i].geometry.coordinates);
+		for(var i=0; i<uniqueVertices.length ; i++){
+			coordContourLines.push(uniqueVertices[i].geometry.coordinates);
 		}
 		contourLines[z]=turf.lineString(coordContourLines, {"z" : z});
 		z+=2;
@@ -74,4 +76,16 @@ function getCoordsForZ(z, vertex1, vertex2, zmin, zmax){
 	coords[0]=ratioZ*(vertex2[0]-vertex1[0])+vertex1[0];
 	coords[1]=ratioZ*(vertex2[1]-vertex1[1])+vertex1[1];
 	return coords;
+}
+
+function verticesUnique(points) {
+    var cleaned = [];
+    points.forEach(function(itm) {
+        var unique = true;
+        cleaned.forEach(function(itm2) {
+            if (_.isEqual(itm, itm2)) unique = false;
+        });
+        if (unique)  cleaned.push(itm);
+    });
+    return cleaned;
 }
